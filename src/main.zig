53295140dda6eaf48f6010eb8ae6696e691c79c9
Richard Hawkins <hurricanerix@shockwave.local>
2026-06-13 08:48:49 -0500

    Show GLFW window

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,34 @@
 const std = @import("std");
+const c = @cImport({
+    @cInclude("GLFW/glfw3.h");
+});
 
 pub fn main() !void {
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    if (c.glfwInit() != c.GLFW_TRUE) {
+        std.debug.print("GLFW failed to initialize\n", .{});
+        return;
+    }
+    defer c.glfwTerminate();
+
+    std.debug.print("GLFW initialized\n", .{});
+
+    c.glfwWindowHint(c.GLFW_CLIENT_API, c.GLFW_NO_API);
+
+    const window = c.glfwCreateWindow(640, 480, "Hello Triangle", null, null);
+    if (window == null) {
+        std.debug.print("GLFW failed to create window\n", .{});
+        return;
+    }
+
+    std.debug.print("GLFW created window\n", .{});
+
+    while (c.glfwWindowShouldClose(window) == c.GLFW_FALSE) {
+        // TODO: Render here
+
+        // TODO: Swap front and back buffers
+
+        c.glfwPollEvents();
+    }
 }
